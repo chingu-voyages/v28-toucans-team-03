7786733a5911a9ea258b96bc84a590c293980005
jazmand <atothezmand@gmail.com>
2021-03-07 01:56:06 +0900

    Add interactive map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-$(document).ready(function () {
-	alert("This is a test.");
+jQuery(document).ready(function () {
+	jQuery("#vmap").vectorMap({
+		map: "world_en",
+		backgroundColor: "#a5bfdd",
+		borderColor: "#818181",
+		borderOpacity: 0.25,
+		borderWidth: 1,
+		color: "#f4f3f0",
+		enableZoom: true,
+		hoverColor: "#c9dfaf",
+		hoverOpacity: null,
+		normalizeFunction: "linear",
+		scaleColors: ["#b6d6ff", "#005ace"],
+		selectedColor: "#c9dfaf",
+		selectedRegions: null,
+		showTooltip: true,
+		onRegionClick: function (element, code, region) {
+			var message =
+				'You clicked "' +
+				region +
+				'" which has the code: ' +
+				code.toUpperCase();
+
+			alert(message);
+		},
+	});
 });
