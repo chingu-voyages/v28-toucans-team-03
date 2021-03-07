ca0cd72761a698873f1a4605b9a76fc5b2ac95f2
yui_murayama <yuimurayama1002@gmail.com>
2021-03-07 22:56:40 +0900

    add API features connecting world map to covid data

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
-$(document).ready(function () {
-	alert("This is a test.");
-	getMapImage();
+'use strict';
+// Get elements from HTML
+const infectedNumAll = document.getElementById('infectedNum__all');
+const infectedNumToday = document.getElementById('infectedNum__today');
+const recoveredNumAll = document.getElementById('recoveredNum__all');
+const recoveredNumToday = document.getElementById('recoveredNum__today');
+const deadNumall = document.getElementById('deadNum__all');
+const deadNumToday = document.getElementById('deadNum__today');
+
+jQuery(document).ready(function () {
+	jQuery("#vmap").vectorMap({
+		map: "world_en",
+		backgroundColor: "#a5bfdd",
+		borderColor: "#818181",
+		borderOpacity: 0.25,
+		borderWidth: 1,
+		color: "#f4f3f0",
+		enableZoom: true,
+		hoverColor: "#c9dfaf",
+		hoverOpacity: null,
+		normalizeFunction: "linear",
+		scaleColors: ["#b6d6ff", "#005ace"],
+		selectedColor: "#c9dfaf",
+		selectedRegions: null,
+		showTooltip: true,
+		onRegionClick: function (element, code, region) {
+			var message =
+				'You clicked "' +
+				region +
+				'" which has the code: ' +
+				code.toUpperCase();
+
+			alert(message);
+
+			// Fetch data from API
+			fetch(`https://corona.lmao.ninja/v2/countries/${code.toUpperCase()}`)
+			.then(res => res.json())
+			.then(data => {
+				// console.log(data);
+				updateData(data);
+			})
+		},
+	});
 });
 
-function getMapImage() {
-	const MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
-	const Options = {
-	zoom: 1,      //地図の縮尺値
-	center: MyLatLng,    //地図の中心座標
-	mapTypeId: 'roadmap'   //地図の種類
-	};
-	const map = new google.maps.Map(document.getElementById('map'), Options);
+function updateData(data) {
+	// Update number
+	infectedNumAll.innerText = 'Number of infected: ' + data.cases;
+	infectedNumToday.innerText = 'Number of recoveries: ' + data.todayCases;
 }
+
