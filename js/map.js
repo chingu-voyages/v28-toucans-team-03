032db9eb3d2c2c11e030924ac9aefdd5c13fb3f7
jazmand <atothezmand@gmail.com>
2021-03-09 12:16:09 +0900

    Add all api get values, bootstrap header, and changed map mouse hover appearance

--- js/map.js
+++ js/map.js
@@ -1,14 +1,39 @@
-let output = document.querySelector(".body__elem");
-let output2 = document.querySelector(".body__elem-2");
 fetch("https://api.covid19api.com/summary")
 	.then((response) => {
 		return response.json();
 	})
 	.then((data) => {
 		console.log(data);
-		let { TotalConfirmed, TotalDeaths, Date } = data.Global;
-		Date = Date.slice(0, 10);
-		output.textContent = `Total confirmed infections globally is ${TotalConfirmed}, while total deaths is ${TotalDeaths} as of ${Date}.`;
+		let globalInfectedTotal = document.querySelector(
+			".global__infected__total"
+		);
+		let globalInfectedNew = document.querySelector(".global__infected__new");
+		let globalDeathsTotal = document.querySelector(".global__deaths__total");
+		let globalDeathsNew = document.querySelector(".global__deaths__new");
+		let globalRecoveredTotal = document.querySelector(
+			".global__recovered__total"
+		);
+		let globalRecoveredNew = document.querySelector(".global__recovered__new");
+		let globalReportedDate = document.querySelector(".global__reported-date");
+		let {
+			TotalConfirmed,
+			NewConfirmed,
+			TotalDeaths,
+			NewDeaths,
+			TotalRecovered,
+			NewRecovered,
+			Date,
+		} = data.Global;
+		globalInfectedTotal.textContent = `Total confirmed global infected: ${TotalConfirmed}`;
+		globalInfectedNew.textContent = `New confirmed global infected: ${NewConfirmed}`;
+		globalDeathsTotal.textContent = `Total confirmed global deaths: ${TotalDeaths}`;
+		globalDeathsNew.textContent = `New confirmed global deaths: ${NewDeaths}`;
+		globalRecoveredTotal.textContent = `Total confirmed global recovered: ${TotalRecovered}`;
+		globalRecoveredNew.textContent = `New confirmed global recovered: ${NewRecovered}`;
+		Day = Date.slice(0, 10);
+		Time = Date.slice(11);
+		Date = `${Day} ${Time}`;
+		globalReportedDate.textContent = `Reported Date: ${Date}`;
 	});
 jQuery(document).ready(function () {
 	jQuery("#vmap").vectorMap({
@@ -32,12 +57,50 @@ jQuery(document).ready(function () {
 					return response.json();
 				})
 				.then((data) => {
+					let countryInfectedTotal = document.querySelector(
+						".country__infected__total"
+					);
+					let countryInfectedNew = document.querySelector(
+						".country__infected__new"
+					);
+					let countryDeathsTotal = document.querySelector(
+						".country__deaths__total"
+					);
+					let countryDeathsNew = document.querySelector(
+						".country__deaths__new"
+					);
+					let countryRecoveredTotal = document.querySelector(
+						".country__recovered__total"
+					);
+					let countryRecoveredNew = document.querySelector(
+						".country__recovered__new"
+					);
+					let countryReportedDate = document.querySelector(
+						".country__reported-date"
+					);
 					data.Countries.forEach((item) => {
 						country = region;
 						if (item.Country == country) {
-							let { TotalConfirmed, TotalDeaths, Date } = item;
-							Date = Date.slice(0, 10);
-							output2.textContent = `Total confirmed infections in ${country} is ${TotalConfirmed}, while total deaths is ${TotalDeaths} as of ${Date}.`;
+							let {
+								TotalConfirmed,
+								NewConfirmed,
+								TotalDeaths,
+								NewDeaths,
+								TotalRecovered,
+								NewRecovered,
+								Date,
+							} = item;
+
+							countryInfectedTotal.textContent = `Total confirmed ${country} infected: ${TotalConfirmed}`;
+							countryInfectedNew.textContent = `New confirmed ${country} infected: ${NewConfirmed}`;
+							countryDeathsTotal.textContent = `Total confirmed ${country} deaths: ${TotalDeaths}`;
+							countryDeathsNew.textContent = `New confirmed ${country} deaths: ${NewDeaths}`;
+							countryRecoveredTotal.textContent = `Total confirmed ${country} recovered: ${TotalRecovered}`;
+							countryRecoveredNew.textContent = `Total confirmed ${country} recovered: ${NewRecovered}`;
+							Day = Date.slice(0, 10);
+							Time = Date.slice(11);
+							Date = `${Day} ${Time}`;
+							countryReportedDate.textContent = `Reported Date: ${Date}`;
 						}
 					});
 				});
