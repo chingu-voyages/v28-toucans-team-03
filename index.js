25d662ec606c57ff42c1b747bc98bf32fd33bfb9
jazmand <atothezmand@gmail.com>
2021-03-18 02:53:12 +0900

    converted html files to ejs files and refactor code

--- index.js
+++ index.js
@@ -16,57 +16,89 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Static Files
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
+app.use("/css", express.static(__dirname + "./public/css"));
+app.use("/js", express.static(__dirname + "./public/js"));
+
+//Set Template Engine
+app.set("view engine", "ejs");
 
 // All Routes
-// Default
-app.get("/news", async (req, res) => {
-  const covidUrl = `https://newsapi.org/v2/everything?q=covid&apiKey=${apiKey}`;
-  const covidResponse = await fetch(covidUrl);
-  const covidData = await covidResponse.json();
 
-  res.json(covidData);
+//ROUTES//
+app.get("/", (req, res) => {
+	res.render("index", { title: "Home Page", page_name: "home" });
+});
+
+app.get("/staying-safe", (req, res) => {
+	res.render("staying-safe", {
+		title: "Staying Safe",
+		page_name: "staying-safe",
+	});
+});
+
+app.get("/self-care", (req, res) => {
+	res.render("self-care", { title: "Self-Care", page_name: "self-care" });
+});
+
+app.get("/news", (req, res) => {
+	res.render("news", { title: "News", page_name: "news" });
+});
+
+//NEWS.JSON ROUTES//
+// Default
+
+app.get("/news.json", async (req, res) => {
+	const covidUrl = `https://newsapi.org/v2/everything?q=covid&apiKey=${apiKey}`;
+	const covidResponse = await fetch(covidUrl);
+	const covidData = await covidResponse.json();
+
+	res.json(covidData);
 });
 
 // Search by specific keyword
-app.get("/news/:keyword", async (req, res) => {
-  const data = req.params;
-  const searchUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&apiKey=${apiKey}`;
-  const searchResponse = await fetch(searchUrl);
-  const searchData = await searchResponse.json();
+app.get("/news.json/:keyword", async (req, res) => {
+	const data = req.params;
+	const searchUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&apiKey=${apiKey}`;
+	const searchResponse = await fetch(searchUrl);
+	const searchData = await searchResponse.json();
 
-  res.json(searchData);
+	res.json(searchData);
 });
 
 // Search by relevancy
-app.get("/news/relevancy/:keyword", async (req, res) => {
-  const data = req.params;
-  const relevancyUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=relevancy&apiKey=${apiKey}`;
-  const relevancyResponse = await fetch(relevancyUrl);
-  const relevancyData = await relevancyResponse.json();
+app.get("/news.json/relevancy/:keyword", async (req, res) => {
+	const data = req.params;
+	const relevancyUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=relevancy&apiKey=${apiKey}`;
+	const relevancyResponse = await fetch(relevancyUrl);
+	const relevancyData = await relevancyResponse.json();
 
-  res.json(relevancyData);
+	res.json(relevancyData);
 });
 
 // Search by popularity
-app.get("/news/popularity/:keyword", async (req, res) => {
-  const data = req.params;
-  const popularityUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=popularity&apiKey=${apiKey}`;
-  const popularityResponse = await fetch(popularityUrl);
-  const popularityData = await popularityResponse.json();
+app.get("/news.json/popularity/:keyword", async (req, res) => {
+	const data = req.params;
+	const popularityUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=popularity&apiKey=${apiKey}`;
+	const popularityResponse = await fetch(popularityUrl);
+	const popularityData = await popularityResponse.json();
 
-  res.json(popularityData);
+	res.json(popularityData);
 });
 
 // Search by newest
-app.get("/news/newest/:keyword", async (req, res) => {
-  const data = req.params;
-  const newestUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=publishedAt&apiKey=${apiKey}`;
-  const newestResponse = await fetch(newestUrl);
-  const newestData = await newestResponse.json();
+app.get("/news.json/newest/:keyword", async (req, res) => {
+	const data = req.params;
+	const newestUrl = `https://newsapi.org/v2/everything?q=${data.keyword}&sortBy=publishedAt&apiKey=${apiKey}`;
+	const newestResponse = await fetch(newestUrl);
+	const newestData = await newestResponse.json();
 
-  res.json(newestData);
+	res.json(newestData);
 });
 
-app.listen(process.env.PORT, () => console.log("app is running"));
+//Listen on port located in .env
+app.listen(process.env.PORT, () =>
+	console.log(`App listening on port http://localhost:${process.env.PORT}/`)
+);
--- public/js/news.js
+++ public/js/news.js
@@ -8,88 +8,88 @@ let selectedKeyword;
 
 // Default action
 document.addEventListener("DOMContentLoaded", () => {
-  getData();
+	getData();
 });
 
 // User search by keyword
 searchButton.addEventListener("click", async () => {
-  selectedKeyword = searchInput.value;
-  const apiUrl = `news/${selectedKeyword}`;
-  const response = await fetch(apiUrl);
-  const data = await response.json();
+	selectedKeyword = searchInput.value;
+	const apiUrl = `news.json/${selectedKeyword}`;
+	const response = await fetch(apiUrl);
+	const data = await response.json();
 
-  renderArticles(data);
+	renderArticles(data);
 });
 
 // User choose from the selectbox
 sortSelect.onchange = async function () {
-  selectedKeyword = searchInput.value;
-  const selectedNumber = Number(sortSelect.value);
+	selectedKeyword = searchInput.value;
+	const selectedNumber = Number(sortSelect.value);
 
-  if (selectedNumber === 1) {
-    const apiUrl = `news/relevancy/${selectedKeyword}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+	if (selectedNumber === 1) {
+		const apiUrl = `news.json/relevancy/${selectedKeyword}`;
+		const response = await fetch(apiUrl);
+		const data = await response.json();
 
-    renderArticles(data);
-    return;
-  }
+		renderArticles(data);
+		return;
+	}
 
-  if (selectedNumber === 2) {
-    const apiUrl = `news/popularity/${selectedKeyword}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+	if (selectedNumber === 2) {
+		const apiUrl = `news.json/popularity/${selectedKeyword}`;
+		const response = await fetch(apiUrl);
+		const data = await response.json();
 
-    renderArticles(data);
-    return;
-  }
+		renderArticles(data);
+		return;
+	}
 
-  if (selectedNumber === 3) {
-    const apiUrl = `news/newest/${selectedKeyword}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+	if (selectedNumber === 3) {
+		const apiUrl = `news.json/newest/${selectedKeyword}`;
+		const response = await fetch(apiUrl);
+		const data = await response.json();
 
-    renderArticles(data);
-    return;
-  }
+		renderArticles(data);
+		return;
+	}
 };
 
 async function getData() {
-  const response = await fetch("/news");
-  const data = await response.json();
+	const response = await fetch("/news.json");
+	const data = await response.json();
 
-  renderArticles(data);
+	renderArticles(data);
 }
 
 function renderArticles(data) {
-  if (articleContainer.firstChild) {
-    hideArticles();
-  }
-  for (let i = 0; i < data.articles.length; i++) {
-    // Container that wraps title, content and link
-    const article = document.createElement("div");
+	if (articleContainer.firstChild) {
+		hideArticles();
+	}
+	for (let i = 0; i < data.articles.length; i++) {
+		// Container that wraps title, content and link
+		const article = document.createElement("div");
 
-    // Title parts
-    const title = document.createElement("h3");
-    title.textContent = data.articles[i].title;
+		// Title parts
+		const title = document.createElement("h3");
+		title.textContent = data.articles[i].title;
 
-    // Content parts
-    const content = document.createElement("p");
-    content.textContent = data.articles[i].content;
+		// Content parts
+		const content = document.createElement("p");
+		content.textContent = data.articles[i].content;
 
-    // Button to read full content
-    const link = document.createElement("a");
-    link.href = data.articles[i].url;
-    link.textContent = "Read all";
+		// Button to read full content
+		const link = document.createElement("a");
+		link.href = data.articles[i].url;
+		link.textContent = "Read all";
 
-    article.append(title, content, link);
-    articleContainer.append(article);
-  }
+		article.append(title, content, link);
+		articleContainer.append(article);
+	}
 }
 
 // Remove the articles if excists
 function hideArticles() {
-  while (articleContainer.firstChild) {
-    articleContainer.removeChild(articleContainer.firstChild);
-  }
+	while (articleContainer.firstChild) {
+		articleContainer.removeChild(articleContainer.firstChild);
+	}
 }
