61171c20f5897fcf8e8211e5216561cb1cff9ef1
yui_murayama <yuimurayama1002@gmail.com>
2021-03-20 19:23:18 +0900

    add new style on articles

--- public/js/news.js
+++ public/js/news.js
@@ -5,6 +5,8 @@ const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
 const sortSelect = document.getElementById("sortSelect");
 let selectedKeyword;
+let rows = []
+let articles = []
 
 // Default action
 document.addEventListener("DOMContentLoaded", () => {
@@ -58,6 +60,7 @@ async function getData() {
 	const response = await fetch("/news.json");
 	const data = await response.json();
 
+	console.log(data);
 	renderArticles(data);
 }
 
@@ -65,26 +68,49 @@ function renderArticles(data) {
 	if (articleContainer.firstChild) {
 		hideArticles();
 	}
-	for (let i = 0; i < data.articles.length; i++) {
-		// Container that wraps title, content and link
+	
+	for(let i = 0; i < data.articles.length / 4; i++) {
+		const row = document.createElement('div')
+		row.setAttribute('class', `row row-${i}`)
+
+		rows.push(row)
+	}
+
+	for(let i = 0; i < data.articles.length; i++) {
+
 		const article = document.createElement("div");
+		article.classList.add('col', `col-${i}`)
 
 		// Title parts
 		const title = document.createElement("h3");
+		title.setAttribute('class', `title title-${i}`)
 		title.textContent = data.articles[i].title;
 
 		// Content parts
 		const content = document.createElement("p");
+		content.setAttribute('class', `content content-${i}`)
 		content.textContent = data.articles[i].content;
 
 		// Button to read full content
 		const link = document.createElement("a");
+		link.setAttribute('class', `link link-${i}`)
 		link.href = data.articles[i].url;
 		link.textContent = "Read all";
 
 		article.append(title, content, link);
-		articleContainer.append(article);
+
+		articles.push(article)
 	}
+
+	for(let i = 0; i < rows.length; i++) {
+		const eachRow = articles.splice(0, 4)
+		rows[i].append(eachRow[0], eachRow[1], eachRow[2], eachRow[3])
+
+		articleContainer.append(rows[i])
+		
+	}
+
+	console.log(articleContainer);
 }
 
 // Remove the articles if excists
